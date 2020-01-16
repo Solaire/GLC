0eee17e32c5caf7f02b6cdd3aa87858a8f4a05fa
kj <kjurczak97@yahoo.co.uk>
2020-01-16 21:12:34 +0000

    Tried a JSON.Net serializer and it turns out it's not working for me (shame). I will write my own JSON serializer (similar to the C++ Json Wrapper class) - the Json.NET was an external dependency, and I'd like to use as few of these as possible.

---
+++ GameLauncher_Console/Json.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace GameLauncher_Console
+{
+	class CJsonWrapper
+	{
+		class TestObject
+		{
+			public int		intItem { get; set; }
+			public string	strItem { get; set; }
+			public bool		boolItem { get; set; }
+		}
+	}
+}
--- GameLauncher_Console/Program.cs
+++ GameLauncher_Console/Program.cs
@@ -14,8 +14,9 @@ namespace GameLauncher_Console
 			AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(Logger.CLogger.ExceptionHandleEvent);
 			Logger.CLogger.Configure("GameLauncherConsole.log"); // Create a log file
 
-			CConsole console = new CConsole(3, 15, CConsole.ConsoleState.cState_Navigate);
-			console.ConsoleStart();
+			//CConsole console = new CConsole(3, 15, CConsole.ConsoleState.cState_Navigate);
+			//console.ConsoleStart();
+			CJsonWrapper json = new CJsonWrapper();
 		}
 	}
 }
