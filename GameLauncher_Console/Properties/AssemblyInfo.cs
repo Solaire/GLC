63c4a5dabed6259eaf97fc80c37e22003099b9b7
kj <kjurczak97@yahoo.co.uk>
2020-03-13 22:27:58 +0000

    Downgraded to .net v4.6.1 (minumum) and restored assembly merger

--- GameLauncher_Console/Properties/AssemblyInfo.cs
+++ GameLauncher_Console/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GameLauncher")]
-[assembly: AssemblyCopyright("Copyright © kaz-jurczak 2020")]
+[assembly: AssemblyCopyright("Copyright © kaz 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
