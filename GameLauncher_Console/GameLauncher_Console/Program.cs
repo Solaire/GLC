6a1044551d132fc8f34aadefbf1e03211c7e691e
Nutzzz <enussbaum@hotmail.com>
2022-09-22 15:19:55 -0700

    Update Program.cs

--- GameLauncher_Console/GameLauncher_Console/Program.cs
+++ GameLauncher_Console/GameLauncher_Console/Program.cs
@@ -28,8 +28,10 @@ namespace GameLauncher_Console
     {
 		private const string IMAGE_FOLDER_NAME = "CustomImages";
 		private const string GAME_FOLDER_NAME = "CustomGames";
+		private const string MIN_WINDOWS_VER = "10.0.17763.0";
+		private const string MIN_WINDOWS_NAME = "Windows 10 1809";
 
-		[STAThread] // Requirement for Shell32.Shell [and Microsoft.Web.WebView2] COM objects
+        [STAThread] // Requirement for Shell32.Shell [and Microsoft.Web.WebView2] COM objects
 		public static void Main(string[] args)
         {
 #if DEBUG
@@ -61,10 +63,10 @@ namespace GameLauncher_Console
 			// Allow for unicode characters, such as trademark symbol
 			Console.OutputEncoding = System.Text.Encoding.UTF8;  // 'The handle is invalid.'
 
-            if (OperatingSystem.IsWindows() && Environment.OSVersion.Version < Version.Parse("10.0.17763.0"))
+            if (OperatingSystem.IsWindows() && Environment.OSVersion.Version < Version.Parse(MIN_WINDOWS_VER))
             {
-                CLogger.LogInfo("Windows 10 1809 or greater required.");
-				Console.WriteLine("Windows 10 1809 or greater required.");
+                CLogger.LogInfo(MIN_WINDOWS_NAME + " or greater required.");
+				Console.WriteLine(MIN_WINDOWS_NAME + " or greater required.");
 				Environment.Exit(-1);
             }
 
